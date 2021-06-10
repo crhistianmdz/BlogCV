f121e39934b833df9aa696e08b8453abdf284b59
Crhistian Mendoza <crhistianmendoza5@gmail.com>
2021-06-10 12:44:22 -0500

    modificado el nombre de clases basadas en vistas anteiores, creada el update para los posts completo

--- post/urls.py
+++ post/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns=[
     path('',views.PostListView.as_view(),name='list_post'),
     path('post/<slug:slug>', views.PostDetailView.as_view()),
-    path('post/new/',views.PostView.as_view(),name='new_post'),
+    path('post/new/',views.PostNewView.as_view(),name='new_post'),
+    path('post/<slug:slug>/edit',views.PostUpdateViews.as_view()),
 ]
--- post/views.py
+++ post/views.py
@@ -9,6 +9,7 @@ from . import forms
 
 from . import models
 
+
 class PostListView(ListView):
     model=models.Post
 
@@ -16,18 +17,41 @@ class PostDetailView(View):
     model=models.Post
     form_class=forms.PostForm
     template_name='post/post.html'
-    def get(self,request,*args,**kwargs):
+    def get(self,request,**kwargs):
         post=get_object_or_404(models.Post,**kwargs)
         return render(request,self.template_name,{'post':post})
-class PostView(View):
+
+class PostNewView(View):
     model=models.Post
     form_class=forms.PostForm
     template_name='post/post_form.html'
     def get(self,request):
         return render(request,self.template_name,{'form':self.form_class})
     
-    def post(self,request,*args,**kwargs):
+    def post(self,request):
         form=forms.PostForm(data=request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                messages.success(request,'Post creado con exito')
+                return redirect(to='list_post')
+            except IntegrityError:
+                messages.error(request,'No se pudo cargar el Post, intente nuevamente')
+                return redirect(to='new_post')
+        messages.error(request,'Algo a salido mal, por intenta nuevamente')
+        return redirect(to='new_post')
+
+class PostUpdateViews(View):
+    model=models.Post
+    template_name='post/post_form.html'
+    form_class=forms.PostForm
+    def get(self,request,**kwargs):
+        post=get_object_or_404(models.Post,**kwargs)
+        form=self.form_class(instance=post)
+        return render(request,self.template_name,{'form':form})
+    def post(self,request):
+        post=get_object_or_404(models.Post,slug=request.POST['slug'])
+        form=self.form_class(data=request.POST,instance=post)
         if form.is_valid():
             try:
                 form.save()

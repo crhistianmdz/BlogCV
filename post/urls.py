e4e8f40f38a05ce7c563d1d37419d1d4b4a67347
Crhistian Mendoza <crhistianmendoza5@gmail.com>
2021-06-10 10:55:58 -0500

    se creo el la classe de postView para trabajar con las peticiones de los post, metodo get y post trabanajando, creada el template y usado el formulario de post

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,10 @@
+from post.models import Post
 from django.urls import path
 from . import views
 
 
 urlpatterns=[
-    path('',views.PostListView.as_view(),name='list'),
+    path('',views.PostListView.as_view(),name='list_post'),
     path('post/<slug:slug>', views.PostDetailView.as_view()),
+    path('post/new/',views.PostView.as_view(),name='new_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.views.generic import ListView
 from django.views import View
+from django.db import IntegrityError
+
 from . import forms
 
 from . import models
@@ -17,3 +19,22 @@ class PostDetailView(View):
     def get(self,request,*args,**kwargs):
         post=get_object_or_404(models.Post,**kwargs)
         return render(request,self.template_name,{'post':post})
+class PostView(View):
+    model=models.Post
+    form_class=forms.PostForm
+    template_name='post/post_form.html'
+    def get(self,request):
+        return render(request,self.template_name,{'form':self.form_class})
+    
+    def post(self,request,*args,**kwargs):
+        form=forms.PostForm(data=request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                messages.success(request,'Post creado con exito')
+                return redirect(to='list_post')
+            except IntegrityError:
+                messages.error(request,'No se pudo cargar el Post, intente nuevamente')
+                return redirect(to='new_post')
+        messages.error(request,'Algo a salido mal, por intenta nuevamente')
+        return redirect(to='new_post')
